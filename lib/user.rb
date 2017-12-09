ca36d6c81161070eabf9781c37efef42914f49ea
Ryan Schmelter <rschmelter33@gmail.com>
2017-12-09 20:51:53 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User
   attr_accessor :first_name, :last_name
 
-  def initialize
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
   end
-  
+
 
 end
